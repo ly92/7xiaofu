30aeaff1f144e198dd2d2f8dcc05671121ac2146
ly <1364757394@qq.com>
2017-05-26 14:00:11 +0800

    fix bug

--- QiXiaoFuProject/code/view/接发单/子页面/发单/controller/SendOrderViewController.m
+++ QiXiaoFuProject/code/view/接发单/子页面/发单/controller/SendOrderViewController.m
@@ -394,11 +394,9 @@
          }
         if (indexPath.row ==4) {// 预约开始时间
             
-            STPickerDate *pickerDate = [[STPickerDate alloc]initWithRow:3];
-            pickerDate.pickerDate3Block = ^(NSInteger year,NSInteger month,NSInteger day,NSString * time){
- 
-                
-                cell.descLab.text =[NSString stringWithFormat:@"%ld年%ld月%ld日",year,month,day];
+            STPickerDate *pickerDate = [[STPickerDate alloc]initWithRow:5];
+            pickerDate.pickerDate5Block = ^(NSInteger year,NSInteger month,NSInteger day,NSInteger hour,NSInteger minute,NSString * time){
+                cell.descLab.text =[NSString stringWithFormat:@"%ld年%ld月%ld日 %ld:%ld",year,month,day,hour,minute];
                 _requestParams[@"service_stime"] = [Utool timestampForDateFromString:time withFormat:@"yyyy.MM.dd HH:mm"];//服务预约开始时间【时间戳】
             };
             [pickerDate show];
@@ -406,9 +404,9 @@
         }
         if (indexPath.row ==5) {// 预约结束时间
             
-            STPickerDate *pickerDate = [[STPickerDate alloc]initWithRow:3];
-            pickerDate.pickerDate3EndBlock = ^(NSInteger year,NSInteger month,NSInteger day,NSString * time){
-                cell.descLab.text =[NSString stringWithFormat:@"%ld年%ld月%ld日",year,month,day];
+            STPickerDate *pickerDate = [[STPickerDate alloc]initWithRow:5];
+            pickerDate.pickerDate5Block = ^(NSInteger year,NSInteger month,NSInteger day,NSInteger hour,NSInteger minute,NSString * time){
+                cell.descLab.text =[NSString stringWithFormat:@"%ld年%ld月%ld日 %ld:%ld",year,month,day,hour,minute];
                 _requestParams[@"service_etime"] = [Utool timestampForDateFromString:time withFormat:@"yyyy.MM.dd HH:mm"];//服务预约结束时间【时间戳】
                 
             };
